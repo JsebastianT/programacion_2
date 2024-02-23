48609b8ae7d15c1d01085d16046a625f9b5f2552
Sebastián Torres <sebastorres325@gmail.com>
2024-02-22 19:49:18 -0500

    ,

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
@@ -1,7 +1,32 @@
 package co.edu.uniquindio.biblioteca;
 
+import co.edu.uniquindio.biblioteca.model.Biblioteca;
+import co.edu.uniquindio.biblioteca.model.Bibliotecario;
+import co.edu.uniquindio.biblioteca.model.Libro;
+
 public class Main {
     public static void main(String[] args) {
+       Biblioteca biblioteca = incializarDatos();
+
+    }
+
+    private static Biblioteca incializarDatos() {
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.setNombre("Pepito");
+
+        Bibliotecario bibliotecario1 = new Bibliotecario();
+        bibliotecario1.setNombre("Hugo");
+        biblioteca.getListaEmpleados().add(bibliotecario1)
+
+        Libro libro1 = new Libro();
+        libro1.setTitulo("Mil y una noche");
+        libro1.setAutor("Sebastián Torres");
+        libro1.setISBM("34567");
+        libro1.setEstado("Ocupado");
+        biblioteca.getListaLibros().add(libro1);
+
+
+
 
     }
 }
